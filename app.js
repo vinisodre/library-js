ec1932b75702e31a4c58a5c86aa9cd82c3f9d012
Vinicius Moraes Sodré <vni217@gmail.com>
2021-07-10 14:36:40 -0300

    push para o github

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const readline = require('readline-sync');
 
 //get input
 const entrada = readline.question('Deseja buscar umm livro?S/N')
+
+//if yes, show categories and ask  to choose one
+if(entrada.toLocaleUpperCase()==='S'){
+    console.log('Escolha uma categoria')
+    console.log('Produtividade', '/História brasileira','/Américas','/Estilo de vida','/Tecnologia')
+}
+
+
+//if no, show all books ordered by page quantity
